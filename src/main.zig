b162abbb88a0b5f72e4812aa6e9b2d018cc6c7d0
Lauri Gustafsson <lauri.gustafsson@iki.fi>
2025-09-19 11:30:47 +0300

    Add cli args struct

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
+const Writer = std.Io.Writer;
 const builtin = @import("builtin");
 
 const c = @cImport({
@@ -15,11 +16,12 @@ const ConfigLoadError =
 
 const Config = struct {
     out_dir: []const u8 = "generated",
-
-    pub const path = "zmarkgen.zon";
+    charset: []const u8 = "utf-8",
+    site_name: ?[]const u8 = null,
 
     pub fn load(
         allocator: Allocator,
+        path: []const u8,
         diagnostics: ?*std.zon.parse.Diagnostics,
     ) ConfigLoadError!Config {
         const file = try std.fs.cwd().openFile(path, .{ .mode = .read_only });
@@ -42,21 +44,83 @@ const Config = struct {
     }
 };
 
+const Args = struct {
+    recursive: bool = false,
+    config_path: []const u8 = "zmarkgen.zon",
+
+    pub fn parse() Args {
+        var result: @This() = .{};
+
+        var args = std.process.args();
+        _ = args.skip();
+        while (args.next()) |arg| {
+            if (std.mem.eql(u8, arg, "-h")) {
+                std.log.info(
+                    \\Example: zmarkgen -r -c doc/zmarkgen.zon -- README.md doc/
+                , .{});
+                std.process.exit(0);
+            } else if (std.mem.eql(u8, arg, "-r")) {
+                result.recursive = true;
+            } else if (std.mem.eql(u8, arg, "-c")) {
+                if (args.next()) |path| {
+                    result.config_path = path;
+                } else {
+                    std.log.err("Option -c requires a file path", .{});
+                    std.process.exit(1);
+                }
+            } else if (std.mem.eql(u8, arg, "--")) {
+                break;
+            } else {
+                std.log.err("Unrecognized argument: {s}", .{arg});
+                std.process.exit(1);
+            }
+        }
+
+        return result;
+    }
+};
+
+fn htmlHead(
+    writer: *Writer,
+    config: Config,
+    title: []const u8,
+) Writer.Error!void {
+    try writer.writeAll("<head>");
+
+    // Add charset
+    try writer.print("<meta charset=\"{s}\">", .{config.charset});
+
+    // Add title and optional site name
+    try writer.print("<title>{s}", .{title});
+    if (config.site_name) |site| {
+        try writer.print(" - {s}", .{site});
+    }
+    try writer.writeAll("</title>");
+
+    try writer.writeAll("</head>");
+}
+
 pub fn main() void {
-    std.log.info("Using cmark {s}", .{c.cmark_version_string()});
+    std.log.debug("Using cmark {s}", .{c.cmark_version_string()});
     var arena = std.heap.ArenaAllocator.init(std.heap.raw_c_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
 
+    const args = Args.parse();
+
     // Load config file
-    std.log.info("Loading {s}", .{Config.path});
+    std.log.info("Loading {s}", .{args.config_path});
     var zon_diagnostics: std.zon.parse.Diagnostics = .{};
     const config: Config = Config.load(
         allocator,
+        args.config_path,
         &zon_diagnostics,
     ) catch |e| switch (e) {
         error.FileNotFound => def: {
-            std.log.info("File {s} not found, using defaults", .{Config.path});
+            std.log.info(
+                "File {s} not found, using defaults",
+                .{args.config_path},
+            );
             break :def .{};
         },
         error.ParseZon => {
@@ -69,8 +133,11 @@ pub fn main() void {
         },
     };
     _ = config;
+
+    // Process sources
+
 }
 
 pub const std_options: std.Options = .{
-    .log_level = if (builtin.mode == .Debug) .debug else .warn,
+    .log_level = if (builtin.mode == .Debug) .debug else .info,
 };
