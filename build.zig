560cb0fc0bee6d4f9596475f4cab8348e84279c7
Lauri Gustafsson <lauri.gustafsson@iki.fi>
2025-09-19 10:06:57 +0300

    Add cmark 0.31.1

--- build.zig
+++ build.zig
@@ -4,19 +4,63 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    // Markdown renderer library: cmark
+    const cmark = b.dependency("cmark", .{});
+    const cmark_mod = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+        .strip = optimize == .ReleaseSmall,
+    });
+
+    cmark_mod.addCSourceFiles(.{
+        .root = cmark.path("src"),
+        .files = &.{
+            "blocks.c",
+            "buffer.c",
+            "cmark.c",
+            "cmark_ctype.c",
+            "commonmark.c",
+            "houdini_href_e.c",
+            "houdini_html_e.c",
+            "houdini_html_u.c",
+            "html.c",
+            "inlines.c",
+            "iterator.c",
+            "latex.c",
+            "man.c",
+            "node.c",
+            "references.c",
+            "render.c",
+            "scanners.c",
+            //"scanners.re",
+            "utf8.c",
+            "xml.c",
+        },
+    });
+
+    const cmark_lib = b.addLibrary(.{
+        .linkage = .static,
+        .name = "cmark",
+        .root_module = cmark_mod,
+    });
+
     // Main executable
     const exe_mod = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
         .strip = optimize == .ReleaseSmall,
+        .link_libc = true,
     });
+
+    exe_mod.addIncludePath(cmark.path("src"));
+    exe_mod.linkLibrary(cmark_lib);
 
     const exe = b.addExecutable(.{
         .name = "zmarkgen",
         .root_module = exe_mod,
     });
-    exe.linkLibC();
     b.installArtifact(exe);
 
     // Run step
--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,13 @@
 const std = @import("std");
+const builtin = @import("builtin");
+const c = @cImport({
+    @cInclude("cmark.h");
+});
 
-pub fn main() void {}
+pub const std_options: std.Options = .{
+    .log_level = if (builtin.mode == .Debug) .debug else .warn,
+};
+
+pub fn main() void {
+    std.log.info("Using cmark {s}", .{c.cmark_version_string()});
+}
